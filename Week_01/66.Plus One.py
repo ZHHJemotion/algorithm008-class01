dcc7e8ea04ce4c1c0973e4438b977a81a3db65b6
Hangjian Zhang <zhhjemotion@hotmail.com>
2020-04-14 20:23:01 +0800

    66. plus one

---
+++ Week_01/66.Plus One.py
@@ -0,0 +1,52 @@
+# Given a non-empty array of digits representing a non-negative integer, plus on
+# e to the integer.
+#
+#  The digits are stored such that the most significant digit is at the head of
+# the list, and each element in the array contain a single digit.
+#
+#  You may assume the integer does not contain any leading zero, except the numb
+# er 0 itself.
+#
+#  Example 1:
+#
+#
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+#
+#
+#  Example 2:
+#
+#
+# Input: [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+#  Related Topics Array
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        flag = 1
+        res = []
+
+        for i in range(len(digits)):
+            # 出栈
+            num = digits.pop() + flag
+            if num >= 10:
+                flag = 1
+                # 入栈
+                res.append(num % 10)
+            else:
+                flag = 0
+                # 入栈
+                res.append(num)
+
+        if flag == 1:
+            res.append(flag)
+
+        # list 反转
+        res.reverse()
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
