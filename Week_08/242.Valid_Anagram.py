4e4df64223232da16a44e8099cdd46f6b9f4b55e
Hangjian Zhang <zhhjemotion@hotmail.com>
2020-06-14 20:57:00 +0800

    242. valid anagram

---
+++ Week_08/242.Valid_Anagram.py
@@ -0,0 +1,40 @@
+# Given two strings s and t , write a function to determine if t is an anagram o
+# f s.
+#
+#  Example 1:
+#
+#
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+#
+#
+#  Example 2:
+#
+#
+# Input: s = "rat", t = "car"
+# Output: false
+#
+#
+#  Note:
+# You may assume the string contains only lowercase alphabets.
+#
+#  Follow up:
+# What if the inputs contain unicode characters? How would you adapt your soluti
+# on to such case?
+#  Related Topics Hash Table Sort
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return sorted(s) == sorted(t)
+
+        res = True
+        if set(s) == set(t):
+            for i in set(s):
+                res = res and s.count(i) == t.count(i)
+        else:
+            return False
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
